3b8c0af2e3d90037ea882f34b3a17ceb6313d325
Aqila Zafira <hartaticpg@gmail.com>
2024-11-05 16:13:22 +0700

    P5

---
+++ Pertemuan05/Praktikum/P5_1_714230068/P5_1_714230068/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_1_714230068
+{
+    public partial class OutpuLabel : Form
+    {
+        public OutpuLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputLabel_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "Hello World";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
